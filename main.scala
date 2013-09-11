48db298b0b6bc0fe0f714b6d9832e749c48c2b42
Thomas Gebert <thomasgebert@gmail.com>
2013-09-11 18:08:40 -0500

    Get rid of unnecessary variable.

--- main.scala
+++ main.scala
@@ -1,7 +1,6 @@
 import scala.collection.parallel 
  object HelloWorld {
     def main(args: Array[String]) {
-     val myList = (1 to 100)
      def fizzBuzzFunction(x:Int):(String, Int) = {
      	 var myString = ""
      	 if(x%3==0) myString +="fizz"
@@ -9,7 +8,7 @@ import scala.collection.parallel
 	 if(x%3!=0 && x%5!=0) myString+=x
 	 return (myString, x)
      }
-     var processedList = myList.par.map(fizzBuzzFunction(_)).toList.sortBy(_._2)
+     var processedList = (1 to 100).par.map(fizzBuzzFunction(_)).toList.sortBy(_._2)
      processedList.map {case(e,f) => println(e)}
     }
   }
