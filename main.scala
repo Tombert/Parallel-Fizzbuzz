f3bb90c1631dfb27611c1e282ba2e8530819cf3c
Thomas Gebert <thomasgebert@gmail.com>
2013-09-10 20:20:26 -0500

    Fix problem with borked order.

--- main.scala
+++ main.scala
@@ -9,7 +9,7 @@ import scala.collection.parallel
 	 if(x%3!=0 && x%5!=0) myString+=x
 	 return (myString, x)
      }
-     var processedList = myList.par.map(fizzBuzzFunction(_))
-     println(processedList)
+     var processedList = myList.par.map(fizzBuzzFunction(_)).toList.sortBy(_._2)
+     processedList.map {case(e,f) => println(e)}
     }
   }
