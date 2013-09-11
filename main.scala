eb376cac470278ce8ec03dfcb6b1adb689181580
Thomas Gebert <thomasgebert@gmail.com>
2013-09-10 19:46:10 -0500

    Main (and only) commit).

---
+++ main.scala
@@ -0,0 +1,15 @@
+import scala.collection.parallel 
+ object HelloWorld {
+    def main(args: Array[String]) {
+     val myList = (1 to 100)
+     def fizzBuzzFunction(x:Int):(String, Int) = {
+     	 var myString = ""
+     	 if(x%3==0) myString +="fizz"
+	 if(x%5==0) myString +="buzz"
+	 if(x%3!=0 && x%5!=0) myString+=x
+	 return (myString, x)
+     }
+     var processedList = myList.par.map(fizzBuzzFunction(_))
+     println(processedList)
+    }
+  }
